c5b4201b111ceb71c0e41ee6d19b645dfd767dd4
Ryan MacInnes <ryan.macinnes@gmail.com>
2022-01-24 15:22:24 -0800

    Viewing playlists.

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,8 +1,27 @@
 class PlaylistsController < ApplicationController
   before_action :authenticate_user!
+
   def index
+    @playlists = interface_request('playlists')
   end
 
   def show
+    @playlist = interface_request('playlist', params[:id])
+    @tracks = @playlist['tracks']['items'].map {|item| item['track']}
+  end
+
+  private
+
+  def interface_request(method, arg = nil)
+    interface = Interface.new(current_user.token, current_user.refresh)
+    if arg
+      interface.send(method, arg)
+    else
+      interface.send(method)
+    end
+  rescue Interface::ExpiredToken
+    token = interface.extend_token!
+    current_user.update token: token
+    retry if token.present?
   end
 end
--- app/models/concerns/interface.rb
+++ app/models/concerns/interface.rb
@@ -1,30 +1,27 @@
 class Interface
-  attr_accessor :token
+  attr_accessor :token, :refresh
 
-  def initialize(token)
+  def initialize(token, refresh)
     @token = token
+    @refresh = refresh
   end
 
-  def like_all_album_songs
-    albums = get 'me/albums'
-
-    limit = 20
-    offset = 0
-    page = 1
-
-    debugger
-    albums['items'].map(&:album).each do |album|
-      tracks = all_tracks(album)
-    end
+  def playlists
+    response = get 'me/playlists', limit: 50
+    response['items']
   end
 
-  def all_tracks(album)
-    debugger
+  def playlist(id)
+    get "playlists/#{id}"
   end
 
   def get(uri, params = {})
-    response = HTTParty.get("https://api.spotify.com/v1/#{uri}", headers: headers, params: params)
-    JSON.parse(response.body)
+    response = HTTParty.get("https://api.spotify.com/v1/#{uri}", headers: headers, query: params)
+    if response.code == 401
+      fail ExpiredToken
+    else
+      JSON.parse(response.body)
+    end
   end
 
   def headers
@@ -32,4 +29,22 @@ class Interface
       "Authorization" => "Bearer " + token
     }
   end
+
+  def extend_token!
+    response = HTTParty.post("https://accounts.spotify.com/api/token?refresh_token", refresh_request)
+    response['access_token']
+  end
+
+  def refresh_request
+    auth = Base64.strict_encode64("#{Rails.application.credentials.spotify[:client_id]}:#{Rails.application.credentials.spotify[:client_secret]}")
+    {
+      body: {
+	grant_type: 'refresh_token',
+	refresh_token: refresh
+      },
+      headers: { 'Authorization' => "Basic #{auth}" }
+    }
+  end
+
+  class ExpiredToken < StandardError; end
 end
