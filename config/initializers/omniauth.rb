16c4aad4ae8ba14767f1f80667da6fc30b1dd288
Ryan MacInnes <ryan.macinnes@gmail.com>
2022-05-23 15:51:19 -0700

    Only requesting necissary omniauth params.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,11 +5,7 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :spotify, Rails.application.credentials.spotify[:client_id], Rails.application.credentials.spotify[:client_secret], scope: %w(
     playlist-read-private
-    user-read-private
     user-read-email
-    user-library-read
-    user-follow-modify
-    user-follow-read
   ).join(' '), provider_ignores_state: true
   provider :ringcentral, Rails.application.credentials.ringcentral[:client_id], Rails.application.credentials.ringcentral[:client_secret]
 end
